a278955fa97002387a5900ca7070162202abbb88
Abhishek Jani <31487386+ajani18@users.noreply.github.com>
2018-09-27 21:15:55 -0400

    Add files via upload

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -12,17 +12,23 @@ public class StringUtils {
             Ex: firstName = "Sean", lastName = "Flannery"
                     --> res = "SFlannery"
          */
+
+        if (lastName.length() >= 8) {
+            lastName = lastName.substring(0, 7);
+
+        }
+
         String res = firstName.substring(0,1) + lastName;
         /* Shorten the string to 8 characters
             Ex: res = "SFlannery"
                     --> res = "SFlanner"
          */
-        res.substring(0, 8);
+        res.substring(0, res.length());
         /* Convert the name to lower-case
             Ex: res = "SFlanner"
                     --> res = "sflanner"
          */
-        res.toLowerCase();
+        res = res.toLowerCase();
         /* return the result of our calculation */
         return res;
     }
